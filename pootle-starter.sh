2c9cf3778a41297501581ef0d1d84c1ab8787a3f
amj <amj@tdct.org>
2017-03-05 21:42:30 +0100

    add db password

--- pootle-starter.sh
+++ pootle-starter.sh
@@ -1,13 +1,12 @@
 chown  1000:1000 /var/www/pootle/.pootle/
-ls -ld /var/www/pootle/.pootle/
-ls -ld /var/www/pootle/
+#ls -ld /var/www/pootle/.pootle/
+#ls -ld /var/www/pootle/
 
 if [ ! -f /var/www/pootle/.pootle/pootle.conf ]; then
 	echo "run pootle init"
 	su-exec pootle ~pootle/env/bin/pootle init --db postgresql --db-name $DB_NAME --db-user $DB_USER --db-host $DB_SERVICE
 	echo "run sed"
-	su-exec pootle sed -e '/#CACHES/,/#}/ s/#\(.*\)/\1/g' -e 's@\(redis://\)127.0.0.1\(:6379\)@\1redis\2@g'  -i ~pootle/.pootle/pootle.conf
-	su-exec pootle echo "LANGUAGE_CODE = 'fr' " >> ~pootle/.pootle/pootle.conf
+	su-exec pootle sed -e "s@\('NAME' *: *\).*@\1'$DB_PASS',@" -e '/#CACHES/,/#}/ s/#\(.*\)/\1/g' -e -e 's@\(redis://\)127.0.0.1\(:6379\)@\1redis\2@g'  -i ~pootle/.pootle/pootle.conf
 	cat ~pootle/.pootle/pootle.conf
 fi
 
