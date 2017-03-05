cd4a49e2b4b444b47a25198cc56946fc78754fe9
amj <amj@tdct.org>
2017-03-05 17:25:31 +0100

    add docker-compose

--- pootle-starter.sh
+++ pootle-starter.sh
@@ -9,4 +9,5 @@ if [ ! -f /var/www/pootle/.initialized ]; then
 	touch /var/www/pootle/.initialized
 fi
 
-~/env/bin/pootle runfcgi host=0.0.0.0 port=8000
+#~/env/bin/pootle runfcgi host=0.0.0.0 port=8000
+~/env/bin/pootle start host=0.0.0.0 port=8000
